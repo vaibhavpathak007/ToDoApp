fdfbd02aa05f69ed67a2a84588b361fe1c74c17d
Vaibhav Pathak <40507@hexaware.com>
2019-02-26 16:47:42 +0530

    Routerlink and pipe added * Added router link on welcome page for navigate to the ToDo page * Added Model class in Todo component as best practice * Used pipes on date field of todo model of todo component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public isCompleted: boolean,
+    public dueDate: Date) {
+  }
+}
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -7,10 +17,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  todos =[
-  {id:1, description: "to learn angular"},
-  {id:2, description: "to learn spring"},
-  {id:3, description: "to learn java"}];
+  todos = [
+    new Todo(1, "to learn angular", true, new Date()),
+    new Todo(2, "to learn spring", false, new Date()),
+    new Todo(3, "to learn java", false, new Date())
+  ];
 
   constructor() { }
 
