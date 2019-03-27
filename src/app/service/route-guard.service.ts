c250285d3a3ff8383198f4c773ab4b037e4c4bca
vaibhavpathak007 <vaibhavpathak007@gmail.com>
2019-03-27 23:28:02 +0530

    Added router redirection to login page.

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { HardcodedauthenticationService } from './hardcodedauthentication.service';
 
 @Injectable({
@@ -7,11 +7,13 @@ import { HardcodedauthenticationService } from './hardcodedauthentication.servic
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private authService: HardcodedauthenticationService) { }
+  constructor(private authService: HardcodedauthenticationService,
+    private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authService.isAuthenticated()) return true;
-    else return false;
+    this.router.navigate(['login']);
+    return false;
   }
 
 }
