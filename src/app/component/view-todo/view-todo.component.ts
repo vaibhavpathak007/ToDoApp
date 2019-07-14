a4e1802da967a02ebeba511a2006cdd414979422
vaibhavpathak007 <vaibhavpathak007@gmail.com>
2019-07-14 21:42:26 +0530

    Addedd Reactive form  Bug - Having Date issue with reactive form.

--- src/app/component/view-todo/view-todo.component.ts
+++ src/app/component/view-todo/view-todo.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../service/todo.service';
 import { Todo } from '../../model/Todo.mode';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-todo',
@@ -10,29 +12,44 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ViewTodoComponent implements OnInit {
 
-  todo: Todo = new Todo(0,'',false, new Date());
+  // todo: Todo = new Todo(0,'',false, new Date());
+  todoForm: FormGroup;
 
   constructor(private todoService: TodoService,
     private router: Router,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.initializeForm();
     let id: number = this.activatedRoute.snapshot.params['id'];
     if(id != 0){
       this.todoService.getTodo('vaibhav',id).subscribe(response=>{
-        this.todo = response;
+        // this.todo = response;
+        this.todoForm.patchValue({
+          id: response.id,
+          description: response.description,
+          date: new DatePipe('en-US').transform(new Date(response.dueDate),'yyyy-MM-dd')
+        });
       });
     }
   }
 
+  initializeForm(){
+    this.todoForm = new FormGroup({
+      id: new FormControl(''),
+      description: new FormControl(''),
+      date: new FormControl('')
+    });
+  }
+
   save(){
-    if(this.todo.id === 0){
-      this.todoService.saveTodo('vaibhav',this.todo).subscribe(response=>{
+    if(this.todoForm.get('id').value === 0){
+      this.todoService.saveTodo('vaibhav',this.todoForm.value).subscribe(response=>{
         console.log(response);
       });
     }
     else{
-      this.todoService.updateTodo('vaibhav',this.todo.id,this.todo).subscribe(response=>{
+      this.todoService.updateTodo('vaibhav',this.todoForm.get('id').value,this.todoForm.value).subscribe(response=>{
         console.log(response);
       });
     }
